e89c5b01043b768f3e2d5d9f54739176313d6b06
Mathieu Comandon <strycore@gmail.com>
2016-02-21 13:46:57 -0800

    Add update-alternatives for vim

--- vim/build.sh
+++ vim/build.sh
@@ -15,3 +15,8 @@ cd vim
             --enable-gui=gtk2 --enable-cscope --prefix=/usr
 make VIMRUNTIMEDIR=/usr/share/vim/vim74
 sudo checkinstall
+
+sudo update-alternatives --install /usr/bin/editor editor /usr/bin/vim 1
+sudo update-alternatives --set editor /usr/bin/vim
+sudo update-alternatives --install /usr/bin/vi vi /usr/bin/vim 1
+sudo update-alternatives --set vi /usr/bin/vim
