c1c5267866c438fa7d3ab1a2d870b4b4c143ff7f
Mathieu Comandon <strycore@gmail.com>
2016-12-07 13:31:59 -0800

    Add build script for Douane

---
+++ douane/build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+sudo apt install liblog4cxx-dev libdbus-c++-dev libgtkmm-3.0-dev
+
+cd
+mkdir Douane
+cd Douane/
+git clone https://github.com/Douane/douane-dkms
+cd douane-dkms
+sudo make dkms
+
+cd ~/Douane
+git clone --recursive https://github.com/Douane/douane-daemon
+cd douane-daemon
+make
+sudo make install
+
+cd ~/Douane
+git clone --recursive https://github.com/Douane/douane-dialog
+cd douane-dialog
+make
+sudo make install
