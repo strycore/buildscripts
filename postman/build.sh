23d5c968f23b7c083154f19d999ce926a2854d78
Mathieu Comandon <strycore@gmail.com>
2017-09-29 19:18:11 -0700

    Add install script for postman gp

---
+++ postman/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudo rm -rf /opt/Postman
+cd /tmp
+wget https://dl.pstmn.io/download/latest/linux64 -O postman.tar.gz
+sudo tar -xzf postman.tar.gz -C /opt
+rm postman.tar.gz
+sudo ln -s /opt/Postman/Postman /usr/local/bin/postman
+
+rm -f ~/.local/share/applications/postman.desktop
+cat > ~/.local/share/applications/postman.desktop <<EOL
+[Desktop Entry]
+Encoding=UTF-8
+Name=Postman
+Exec=postman
+Icon=/opt/Postman/resources/app/assets/icon.png
+Terminal=false
+Type=Application
+Categories=Development;
+EOL
